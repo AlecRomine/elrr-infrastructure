a1f1415d1aee85de3ee6b6785c3d77d476a49e90
micson-us <micson@deloitte.com>
2021-08-10 14:48:34 -0400

    adding lifecycle for kp to prevent destroy

--- terraform/elrr_key_pair.tf
+++ terraform/elrr_key_pair.tf
@@ -1,9 +1,15 @@
 resource "aws_key_pair" "elrr_public_kp" {
   key_name = "elrr_public_kp"
   public_key = file("elrr_public_kp.pub")
+  lifecycle {
+            prevent_destroy = true
+    }
   }
 
 resource "aws_key_pair" "elrr_private_kp" {
   key_name = "elrr_private_kp"
   public_key = file("elrr_private_kp.pub")
+  lifecycle {
+            prevent_destroy = true
+    }
   }
