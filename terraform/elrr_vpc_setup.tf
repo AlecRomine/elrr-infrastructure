77c4327a9bffcb50fb9fe1df632e4774e975807c
micson-us <micson@deloitte.com>
2021-08-16 21:12:12 -0400

    rt update

--- terraform/elrr_vpc_setup.tf
+++ terraform/elrr_vpc_setup.tf
@@ -57,7 +57,8 @@ resource "aws_route_table_association" "elrr_public_rt2" {
 resource "aws_route_table" "elrr_private_route_table" {
   provider = aws
   vpc_id   = aws_vpc.elrr_vpc.id
-  route {
+  route [
+  {
     cidr_block = "10.0.2.0/24"
     nat_gateway_id = aws_nat_gateway.elrr_nat_gateway1.id
   },
@@ -65,6 +66,7 @@ resource "aws_route_table" "elrr_private_route_table" {
     cidr_block = "10.0.12.0/24"
     nat_gateway_id = aws_nat_gateway.elrr_nat_gateway2.id
   }
+]
   tags = {
     Name = "elrr_private_route_table"
   }
